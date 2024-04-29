f09ccc6fbb08fbda739f3d2d3bdab8925e16a3a0
Amaury TISSOT <tissot.amaury@gmail.com>
2024-04-29 14:34:30 +0200

    Create home page layout

---
+++ quizzeo/src/app/home/layout.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/header";
+import Footer from "../components/footer";
+export default function HomeLayout({ children }) {
+    return (
+        <>
+            <Header />
+            {children}
+            <Footer />
+        </>
+    );
+}
