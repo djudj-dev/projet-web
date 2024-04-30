b2e2ff29c04b8de9979bc880847c253a04a79b04
root <dossantosjeff25@gmail.com>
2024-04-30 12:23:32 +0000

    Création de la fonction connexion

--- quizzeo/src/app/api/login/route.js
+++ quizzeo/src/app/api/login/route.js
@@ -1,13 +1,11 @@
 import { NextResponse } from "next/server";
 import { generateJwt } from "../../../lib/jwt-tools";
+import { connexion } from "../../../lib/connexion";
 
 export async function POST (request) {
     console.log(request)
     const { email, password }  = await request.json()
-
-    // add login function
-    // change test by user id
-
-    const jwt = await generateJwt('test')
+    user = connexion(email, password)
+    const jwt = await generateJwt(user.id)
     return NextResponse.json({ jwt }, { status: 404 });
 }
