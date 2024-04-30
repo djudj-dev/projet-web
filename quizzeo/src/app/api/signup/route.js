9ccec19fdf3c57e1189e3144296dcfe81575e409
ijami75 <blackwalls75@gmail.com>
2024-04-30 11:25:07 +0000

    jwt inscription

--- quizzeo/src/app/api/signup/route.js
+++ quizzeo/src/app/api/signup/route.js
@@ -1,12 +1,19 @@
 import { NextResponse } from "next/server";
 import { generateJwt } from "../../../lib/jwt-tools";
+import { signup } from "../../../lib/inscription";
 
 export async function POST (request) {
     console.log(request)
     const { email, password }  = await request.json()
-    // add signup function
-    // change test by user id
-    const jwt = await generateJwt('test');
+    signup(email, password)
+    
+    const user = prisma.user.findUnique({
+        where: {
+            email: email,
+        }
+    })
+
+    const jwt = await generateJwt(user.id);
 
     return NextResponse.json({ jwt });
 }
