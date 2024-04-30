67b167a2c148e2672e29572df49e0cf23ad0ff31
Amaury TISSOT <tissot.amaury@gmail.com>
2024-04-30 15:19:45 +0200

    Modification du CSS pour que le background soit en h-screen

--- quizzeo/src/app/backoffice/layout.jsx
+++ quizzeo/src/app/backoffice/layout.jsx
@@ -2,7 +2,7 @@ import SideBar from "../../components/sidebar";
 
 export default function BackOfficeLayout({ children }) {
     return (
-        <div className="flex justify-center bg-[#F0F0F0]">
+        <div className="flex justify-center bg-[#F0F0F0] h-screen">
             <SideBar />
             {children}
         </div>
