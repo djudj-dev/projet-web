4a92f7e60a82a01ce9003336c5e5b62218d29c2e
Amaury TISSOT <tissot.amaury@gmail.com>
2024-04-30 12:45:16 +0200

    Create Sidebar component

---
+++ quizzeo/src/components/sidebar.jsx
@@ -0,0 +1,77 @@
+import Link from "next/link";
+import Image from "next/image";
+
+//FIXME: Changement de couleur des svg en blanc lors du mouse hover
+const SideBar = () => {
+    return (
+        <>
+            <aside className=" text-[#84602C] flex flex-col justify-between items-center w-[197px] shadow-custom">
+                <div className="flex flex-col justify-center items-center gap-10">
+                    <Image
+                        src="/QuizzeoIcon.svg"
+                        alt="Logo Quizzeo"
+                        className="mt-[37px]"
+                        width={81}
+                        height={96}
+                    />
+                    <nav className="flex flex-col w-[147px] gap-4">
+                        <Link
+                            href="#"
+                            className="py-1 px-1 rounded hover:bg-[#84602C] hover:text-white flex gap-[10px]"
+                        >
+                            <Image
+                                src="/dashboardIcon.svg"
+                                alt=""
+                                className=""
+                                width={18}
+                                height={18}
+                            />
+                            Quiz
+                        </Link>
+                        <Link
+                            href="#"
+                            className="py-1 px-1 rounded hover:bg-[#84602C] hover:text-white flex gap-[10px]"
+                        >
+                            <Image
+                                src="/assignementIcon.svg"
+                                alt=""
+                                className=""
+                                width={18}
+                                height={18}
+                            />
+                            Répondants
+                        </Link>
+                        <Link
+                            href="#"
+                            className="py-1 px-1 rounded hover:bg-[#84602C] hover:text-white flex gap-[10px]"
+                        >
+                            <Image
+                                src="/leaderboardIcon.svg"
+                                alt=""
+                                className=""
+                                width={18}
+                                height={18}
+                            />
+                            Statistiques
+                        </Link>
+                        <Link
+                            href="#"
+                            className="py-1 px-1 rounded hover:bg-[#84602C] hover:text-white flex gap-[10px]"
+                        >
+                            <Image
+                                src="/splitscreenIcon.svg"
+                                alt=""
+                                className=""
+                                width={18}
+                                height={18}
+                            />
+                            Quiz
+                        </Link>
+                    </nav>
+                </div>
+            </aside>
+        </>
+    );
+};
+
+export default SideBar;
