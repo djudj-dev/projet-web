751f14e02d307d0a42e41cee64c408bf407144ee
booder93160 <s.alott@ecole-ipssi.net>
2024-04-30 13:29:57 +0000

    validateur de compte

---
+++ quizzeo/src/lib/Validateur_compte.js
@@ -0,0 +1,122 @@
+import { prismaClientSingleton } from './prisma';
+import bcrypt from 'bcrypt';
+
+const prisma = prismaClientSingleton();
+
+//Vérifier le statut et afficher tous les user
+
+async function checkAllUser(){
+    try {
+        const Alluser = await prisma.findmany ({
+            Select: {
+                id: true,
+                email: true,
+                role: true,
+                enabled: true,
+            }
+
+        })
+
+        return Alluser;
+        
+   
+    } catch (error) {
+        throw error ("Erreur");
+        
+    }
+}
+
+// Vérifier le statut du compte
+
+async function checkAccount(email){
+    try {
+        const account= await prisma.user.findUnique ({
+            where: {
+                email: email,
+            },
+            select: {
+                enabled:true,
+            },
+
+        })
+        
+        return account ; 
+    } catch (error) {
+        throw error ('Erreur wuld a kelb');
+        
+    }
+}
+
+// activer le compte
+
+async function activateAccount(email){
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+              email:email,
+            },
+          });
+          
+          if (user) {
+            if (user.enabled === true) {
+              console.log("Ce compte est déjà actif");
+            } else {
+              await prisma.user.update({
+                where: {
+                  email: email,
+                },
+                data: {
+                  enabled: true,
+                },
+              });
+              console.log("Compte activé avec succès !");
+            }
+          } else {
+            console.log("Utilisateur introuvable");
+          }
+        
+    } catch (error) {
+        throw error (' Non ')
+        
+    }
+}
+
+// Déactiver le compte 
+
+async function DeactivateAccount(email){
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+              email:email,
+            },
+          });
+          
+          if (user) {
+            if (user.enabled === false) {
+              console.log("Ce compte est déjà inactif");
+            } else {
+              await prisma.user.update({
+                where: {
+                  email: email,
+                },
+                data: {
+                  enabled: false,
+                },
+              });
+              console.log("Compte désactivé avec succès !");
+            }
+          } else {
+            console.log("Utilisateur introuvable");
+          }
+        
+    } catch (error) {
+        throw error (' Non ')
+        
+    }
+}
+
+
+
+export{ checkAllUser, checkAccount, activateAccount,DeactivateAccount};
