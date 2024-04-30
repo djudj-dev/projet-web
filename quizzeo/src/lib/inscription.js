7e66fd6d53a0de2e450e67dea5a67b51e9918c6c
ijami75 <blackwalls75@gmail.com>
2024-04-30 09:18:33 +0000

    ajout de la fonction d'inscription

---
+++ quizzeo/src/lib/inscription.js
@@ -0,0 +1,39 @@
+import { prismaClientSingleton } from './prisma';
+import bcrypt from 'bcrypt';
+
+const prisma = prismaClientSingleton();
+
+
+// Fonction d'inscription
+async function signup(login, password) {
+    try {
+        // Vérifier si l'utilisateur existe déjà
+        const existingUser = await prisma.login.findUnique({
+            where: {
+                login: login,
+            },
+        });
+
+        if (existingUser) {
+            throw new Error('Cet identifiant est déjà utilisé.');
+        }
+
+        // Hasher le mot de passe avant de le stocker dans la base de données
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Créer un nouvel utilisateur dans la base de données
+        const newUser = await prisma.user.create({
+            data: {
+                login : login,
+                password: hashedPassword,
+                // Autres champs de l'utilisateur...
+            },
+        });
+
+        return newUser;
+    } catch (error) {
+        throw new Error(Erreur lors de l'inscription : ${error.message});
+    }
+}
+
+export { signup };
