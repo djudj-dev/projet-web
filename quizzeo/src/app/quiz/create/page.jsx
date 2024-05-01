c9872b2486e32a41ced21bf2f9789f85101a7810
AmauryTISSOT <104317431+AmauryTISSOT@users.noreply.github.com>
2024-05-01 16:58:22 +0200

    Modification du CSS et ajout de  texte

--- quizzeo/src/app/quiz/create/page.jsx
+++ quizzeo/src/app/quiz/create/page.jsx
@@ -2,6 +2,15 @@ import { QuizCreatorPage } from "../../../components/quiz-generator";
 
 export default function Page({ params }) {
     return (
+        <div className="flex-auto w-max-[800px text-[#84602C] max-w-3xl mx-auto pt-8 pb-12 font-sans">
+            <section className="text-center my-6">
+                <h1 className="text-3xl font-bold">Créer votre quiz</h1>
+                <p className="mt-6 text-lg">
+                    Créer des quiz en quelques secondes et obtenez un feedback
+                    immédiat et en temps réel <br></br>Gratuit et illimité
+                </p>
+            </section>
             <QuizCreatorPage />
-    )
+        </div>
+    );
 }
