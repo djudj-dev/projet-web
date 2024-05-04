5a9d7538ef78684789cb934a3a3f84136481c0e7
AmauryTISSOT <104317431+AmauryTISSOT@users.noreply.github.com>
2024-05-04 19:49:13 +0200

    Ajout de la feature pour la génération de mot de passe aléatoire

--- quizzeo/src/components/create-user-form.jsx
+++ quizzeo/src/components/create-user-form.jsx
@@ -3,11 +3,23 @@ import { useMutation } from "react-query";
 import { useForm } from "react-hook-form";
 import { postAuthApi } from "../lib/client-fetch";
 import { Spinner } from "./spinner";
+import Image from "next/image";
+import generateSecurePassword from "../lib/passwordGenerator";
+import { useState } from "react";
 
 export const CreateUserForm = ({ user, jwt }) => {
     const { data, error, mutate, isLoading } = useMutation((body) =>
         postAuthApi("auth/user/admin-create", body, jwt)
     );
+
+    // Hook contenant un mot de passe aléatoire
+    const [randomPasswordString, setRandomPasswordString] = useState("");
+
+    // Fonction appelée lorsque l'utilisateur clique sur le bouton de génération d'un mot de passe aléatoire
+    const handleGeneratePassword = () => {
+        const newPassword = generateSecurePassword();
+        setRandomPasswordString(newPassword);
+    };
 
     const { register, handleSubmit } = useForm({
         shouldUseNativeValidation: true,
@@ -33,12 +45,12 @@ export const CreateUserForm = ({ user, jwt }) => {
         >
             {error && (
                 <p className="m-auto text-center p-1 px-3 bg-red-400 w-fit my-2 rounded">
-                    la création d'utilisateur a echouer
+                    La création d'utilisateur a échoué
                 </p>
             )}
             {data && (
                 <p className="m-auto text-center p-1 px-3 bg-green-400 w-fit my-2 rounded">
-                    la création d'utilisateur a reussi
+                    La création d'utilisateur a réussi
                 </p>
             )}
             <div className="mb-6">
@@ -67,15 +79,16 @@ export const CreateUserForm = ({ user, jwt }) => {
                 </label>
                 <input
                     {...register("password")}
-                    type="password"
+                    type="text"
                     id="password"
                     name="password"
+                    value={randomPasswordString}
                     className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
                     placeholder="Entrez votre mot de passe"
                     required
                 />
             </div>
-            <div className="mb-6 flex ">
+            <div className="mb-6 flex gap-3 justify-between">
                 <div className="mr-12">
                     <label
                         htmlFor="newPassword"
@@ -112,6 +125,21 @@ export const CreateUserForm = ({ user, jwt }) => {
                         className="w-[25px] h-[25px] px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500 hover:border-blue-500 mt-1"
                         placeholder="Entrez votre nouveau mot de passe"
                     />
+                </div>
+                <div
+                    className="bg-gray-200 hover:bg-gray-300 font-bold py-2 px-4 rounded border-1 border-gray-600 w-[200px] text-sm text-gray-700 flex gap-3 h-[70px] justify-center items-center"
+                    onClick={handleGeneratePassword}
+                >
+                    <Image
+                        src="/lockResetIcon.svg"
+                        alt=""
+                        className=""
+                        width={30}
+                        height={30}
+                    />
+                    <div className="text-center">
+                        Générer un mot de passe aléatoirement
+                    </div>
                 </div>
             </div>
             <div className="flex justify-center">
